f1d7d356bedb51d4bf0c6715ce22571f13c5fc4e
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-03 19:11:15 -0400

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
+    let emoji = '<:lucatrio:475075813226577938>';
     let embed = new Discord.RichEmbed()
         .setColor('36393F')
         .addField(`Owner`, 'Lucatrio')
-        .setFooter(<:lucatrio:475075813226577938>);
+        .setFooter(emoji);
     message.channel.send(embed);
 }
 
