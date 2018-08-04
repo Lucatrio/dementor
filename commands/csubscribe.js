e7ef9c41ac6f414dcd9acc2ed18db3748892f622
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 13:09:08 -0400

    Update csubscribe.js

--- commands/csubscribe.js
+++ commands/csubscribe.js
@@ -1,17 +1,15 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-  let channel = client.channels.get("475330079426347018");
-  if(message.author.id != '366283647587713034') return;
-  
+  let channel = client.channels.get('475330079426347018');
   let text = args.join(" ");
   let embed = new Discord.RichEmbed()
     .setColor("36393F")
-    .addField(`<@&&475330742080241664>`)
-    .addField(`Certain role only`, text);
-  message.channel.send(embed);
+    .setAuthor(`<@&475330742080241664>`)
+    .addField(`Context`, text);
+    channel.send(embed);
 }
 
-module.exports.help => {
+module.exports.help = {
   name: "csubscribe"
 }
