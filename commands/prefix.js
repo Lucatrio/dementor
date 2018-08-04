dfef30ee949f4f33a6f24bab92e3c0501bd47bb6
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 12:02:26 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,8 +2,10 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 
 module.exports.run = async (client, message, args) => {
-  if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('Sorry, you need the `MANAGE_SERVER` permission to use this');
-  
+  db.fetch(`prefix_${message.guild.id}`).then(i1 => {
+    if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send(`Hello, ${message.author.username} the prefix for this guild is ${i1}. If it says undefined the prefix is **d:**`);
+  })
+    
   db.set(`prefix_${message.guild.id}`, args.join(' ')).then(i => {
     let embed = new Discord.RichEmbed()
       .setColor("36393F")
