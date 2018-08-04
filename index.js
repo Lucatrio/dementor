4f2fd521ebc00ce603b7d8708c417703079f2da8
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 11:35:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({disableEveryone: true});
 const fs = require('fs');
+const db = require('quick.db');
 const prefix = 'd:';
 
 client.on("ready", () => {
