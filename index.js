3d4c786eeebf1b4b76d009b13be241091838b587
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 19:24:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,6 @@ const db = require('quick.db');
 
 client.on("ready", () => {
     console.log(`${client.user.username} has logged on with ${client.users.size} users`);
-    client.user.setActivity('over azkaban', { type: 'WATCHING' });
-});
-
-client.on("message", async message => {
-    
-    let prefix = 'd:';
-    let fetched = await db.fetch(`prefix_${message.guild.id}`);
-    if (fetched === null) prefix = 'd:';
-    else prefix = fetched;
-    
-    if (message.author.bot) return;
-    if(message.content.indexOf(prefix) !== 0) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    try {
-        let commandFile = require(`./commands/${command}`);
-        commandFile.run(client, message, args);
-    } catch (err) {
-        console.log(err);
-    }
+    client.user.setActivity('over my overwrite', { type: 'WATCHING' });
 });
 client.login(process.env.TOKEN)
