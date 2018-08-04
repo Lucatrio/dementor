1a1ea89ff368e6fd57c004152b57f05b2d09155c
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 12:06:44 -0400

    Update unsubscribe.js

--- commands/unsubscribe.js
+++ commands/unsubscribe.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
   if(message.guild.id != '474649915369193484') return;
   let role = message.guild.roles.find('name', 'Subscribed');
-  if(message.member.hasRole(role)) return message.channel.send(`You silly person! You already do not have the role :-:`);
   message.member.removeRole(role);
   message.channel.send(`You have been sucessfully unsubscribed from the dementor showcase guild`);
 }
