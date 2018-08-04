c251597fee9acbf17a4de4ee54a746dfdd4c524a
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 11:34:41 -0400

    Create prefix.js

---
+++ commands/prefix.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+
+module.exports.run = async (client, message, args) => {
+  if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('Sorry, you need the `MANAGE_SERVER` permission to use this');
+  
+  db.set(`prefix_${message.guild.id}`).then(i => {
+    let embed = new Discord.RichEmbed()
+      .setColor("36393F")
+      .addField(`Successfully`, `Overwrited prefix value now value is **${i}**`);
+    message.channel.send(embed);
+  })
+}
+
+module.exports.help = {
+  name: "prefix"
+}
