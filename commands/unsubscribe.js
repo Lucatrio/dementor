65436de3c3e742f40f36d89163c433c6eaaa9180
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 11:59:05 -0400

    Create unsubscribe.js

---
+++ commands/unsubscribe.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+  if(message.guild.id != '474649915369193484') return;
+  let role = message.guild.roles.find('name', 'Subscribed');
+  
+  message.member.removeRole(role);
+  message.channel.send(`You have been sucessfully unsubscribed from the dementor showcase guild`);
+}
+
+module.exports.help = {
+  name: "unsubscribe"
+}
