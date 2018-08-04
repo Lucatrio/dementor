96358064456ff2681773cd9db7312e2958d5063f
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-04 11:55:39 -0400

    Create subscribe.js

---
+++ commands/subscribe.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+  if(message.guild.id != '474649915369193484') return;
+  let role = message.guild.roles.find('name', 'Subscribed');
+  
+  message.member.addRole(role);
+  message.channel.send(`You have been sucessfully subscribed to the dementor showcase guild`);
+}
+
+module.exports.help = {
+  name: "subscribe"
+}
